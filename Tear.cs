0d6fb7e36d2a1cb06c00eda8ff407d9431e6ffa4
P0DG3 <128816362+P0DG3@users.noreply.github.com>
2024-02-01 23:18:32 +0000

    commenting Tear.cs

--- Tear.cs
+++ Tear.cs
@@ -11,12 +11,14 @@ namespace coursework
 {
     class Tear
     {
-        //gonna make an abstraction of the tears from the original game-
-        //by removing the sprite for them and just using some coloured pixels (will change colour and size to indicate stronger/faster tears)
+        //defines the variables for the tear class
+        //will change the colour and maybe the size of the tear later to indicate different tear types
+        //(e.g. red for more damage, bigger -> slower, etc.) 
         public string direction;
         public int tearLeft;
         public int tearTop;
         public bool tearCooldown = true;
+        public int tearDmg = 1;
         Image image = Image.FromFile("tear.png");
 
         private int speed = 25;
@@ -44,7 +46,8 @@ namespace coursework
 
         private void tearTimerEvent(object sender, EventArgs e)
         {
-            //changes the displacement of the tear sprite using the 'speed' variable to update the location of the top and the left of the picture picture box
+            //changes the displacement of the tear sprite using the 'speed' variable
+            //to update the location of the top and the left of the thing.
             if (direction == "Up")
             {
                 tear.Top -= speed;
@@ -64,7 +67,7 @@ namespace coursework
             
 
             //despawn tear and its timer when tear hits any of the edges edges of screen
-            // sets the tear and its timer to null (being memory efficient waow) 
+            // sets the tear and its timer to null (being memory efficient waow). (also doesn't lag the form to shit)
             if (tear.Top < 50 || tear.Top > 950  || tear.Left < 50 || tear.Left > 1800 )
             {
                 tearTimer.Stop();
@@ -72,7 +75,6 @@ namespace coursework
                 tear.Dispose();
                 tearTimer = null;
                 tear = null;
-                
             }
                 
         }
