26d82be5c5d8efb64373875a133856dcade18506
P0DG3 <ollybro379@outlook.com>
2024-03-17 16:06:20 +0000

    Menu system complete (basically)
    Almost fully finished the menu system (just need the game over menu which will have quit, new game, and scores options or can take you to the main menu. This will be done later when I revisit combat and the rest of the game mechanics. but for now main menu works :3

--- ScoreBoard.Designer.cs
+++ ScoreBoard.Designer.cs
@@ -28,16 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(276, 59);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(245, 331);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
             // 
             // ScoreBoard
             // 
@@ -45,7 +36,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::coursework.Properties.Resources._1e1e1e_colour;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
             this.Name = "ScoreBoard";
             this.Text = "ScoreBoard";
             this.Load += new System.EventHandler(this.ScoreBoard_Load);
@@ -54,7 +44,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- ScoreBoard.cs
+++ ScoreBoard.cs
@@ -19,7 +19,17 @@ namespace coursework
 
         private void ScoreBoard_Load(object sender, EventArgs e)
         {
-            
+            // reads the scores from the file and displays them in the rich text box 
+            // it sets a bunch of properties for the rich text box i.e. location, size, font, read only
+            System.IO.File.ReadAllText("Scores.txt");
+            RichTextBox richTextBox = new RichTextBox();
+            richTextBox.Text = System.IO.File.ReadAllText("Scores.txt");
+            richTextBox.Location = new Point(150, 50);
+            richTextBox.Size = new Size(500, 500);
+            richTextBox.Font = new Font("IsaacGame", 14);
+            richTextBox.ReadOnly = true;
+            this.Controls.Add(richTextBox);
+            this.Load += ScoreBoard_Load;
         }
     }
 }
